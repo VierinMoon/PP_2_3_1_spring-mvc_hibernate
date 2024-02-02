913620e4221463f069749346dd78095b0bfdf3fa
VierinMoon <101812476+VierinMoon@users.noreply.github.com>
2024-02-02 16:55:26 +0300

    Created methods int
    Created methods for user interaction on the web

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -3,9 +3,8 @@ package web.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+import web.model.User;
 import web.service.UserService;
 
 @Controller
@@ -19,9 +18,45 @@ public class UserController {
         this.userService = userService;
     }
 
+
     @GetMapping
     public String getUsers(@RequestParam(value = "count", defaultValue = "5") int count,  Model model) {
         model.addAttribute("users", userService.getAllUsers(count));
         return "users";
+    }
+
+    @GetMapping("/{id}")
+    public String getUserById(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "user";
+    }
+
+    @GetMapping("/new")
+    public String newUser(@ModelAttribute("user") User user) {
+        return "new";
+    }
+
+    @PostMapping
+    public String addUser(@ModelAttribute("user") User user) {
+        userService.addUser(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/{id}/update")
+    public String editUser(Model model, @PathVariable("id") Long id) {
+        model.addAttribute("user", userService.getUserById(id));
+        return "update";
+    }
+
+    @PatchMapping("/{id}")
+    public String updateUser(@ModelAttribute("user") User user, @PathVariable("id") Long id) {
+        userService.updateUser(id, user);
+        return  "redirect:/users";
+    }
+
+    @DeleteMapping("/{id}")
+    public String removeUser(@PathVariable("id") Long id) {
+        userService.removeUserById(id);
+        return "redirect:/users";
     }
 }
